cfa38445990b7e669d23b853df2bce295f2bf7ed
Kirill Malevich <kirillmalevich@u.boisestate.edu>
2020-09-17 13:11:51 -0600

    post body

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+var bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 const pool = new Pool({
@@ -11,13 +12,15 @@ const pool = new Pool({
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async (req, res) => {
+  .get('/envdata', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT * FROM test_table');
+      const result = await client.query('SELECT * FROM env_logs');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
       client.release();
@@ -26,8 +29,22 @@ express()
       res.send("Error " + err);
     }
   })
+  .post('/envdata', async (req, res) => {
+    var post_body = req.body;
+    res.send(post_body);
+    // try {
+      
+    //   var queryString = `INSERT INTO env_logs(data) VALUES()`;
+    //   const client = await pool.connect();
+    //   const result = await client.query(queryString);
+    //   client.release();
+    // } catch (err) {
+    //   console.error(err);
+    //   res.send("Error " + err);
+    // }
+  })
   .get('/times', (req, res) => res.send(showTimes()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 showTimes = () => {
   let result = '';
